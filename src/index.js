8bb75f9a18cc536315dafbb23b97a6d92e753202
Ayatarekk <99080000+Ayatarekk@users.noreply.github.com>
2024-10-18 06:31:12 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import BookStoreProvider from "./context/BookStoreProvider";
+import 'bootstrap/dist/css/bootstrap.min.css';
+//  import "~slick-carousel/slick/slick.css"; 
+//  import "~slick-carousel/slick/slick-theme.css";
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BookStoreProvider>
+      <App />
+    </BookStoreProvider>
+  </React.StrictMode>
+);
